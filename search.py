7993a0b0634677054b4c297d09aa4e9af1ddd363
marutoraman <33813153+marutoraman@users.noreply.github.com>
2020-11-06 21:41:18 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -2,16 +2,19 @@ import pandas as pd
 import eel
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
+def kimetsu_search(word,csv_name):
     # 検索対象取得
-    df=pd.read_csv("./source.csv")
+    df=pd.read_csv("./{}".format(csv_name))
     source=list(df["name"])
 
     # 検索
     if word in source:
         print("『{}』はあります".format(word))
+        eel.view_log_js("『{}』はあります".format(word))
     else:
         print("『{}』はありません".format(word))
+        eel.view_log_js("『{}』はありません".format(word))
+        eel.view_log_js("『{}』を追加します".format(word))
         # 追加
         #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
         #if add_flg=="1":
@@ -19,5 +22,5 @@ def kimetsu_search(word):
     
     # CSV書き込み
     df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./source.csv",encoding="utf_8-sig")
+    df.to_csv("./{}".format(csv_name),encoding="utf_8-sig")
     print(source)
--- view.py
+++ view.py
@@ -7,8 +7,8 @@ end_point="index.html"
 size=(700,600)
 
 @ eel.expose
-def kimetsu_search(word):
-    search.kimetsu_search(word)
+def kimetsu_search(word,csv_name):
+    search.kimetsu_search(word,csv_name)
     
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
