024920721937134eff3ca4d405eea85f20b5f1df
Yusuke Koide <yk@MacBook-Pro-2.local>
2021-03-04 23:50:21 +0900

    検索機能の追加

--- search.py
+++ search.py
@@ -2,16 +2,18 @@ import pandas as pd
 import eel
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
+def kimetsu_search(file_name,word):
     # 検索対象取得
-    df=pd.read_csv("./source.csv")
+    df=pd.read_csv(file_name)
     source=list(df["name"])
 
     # 検索
     if word in source:
-        print("『{}』はあります".format(word))
+        print("『{}』はあります\n".format(word))
+        result = "『{}』はあります\n".format(word)
     else:
         print("『{}』はありません".format(word))
+        result = "『{}』はありません".format(word)
         # 追加
         #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
         #if add_flg=="1":
@@ -21,3 +23,4 @@ def kimetsu_search(word):
     df=pd.DataFrame(source,columns=["name"])
     df.to_csv("./source.csv",encoding="utf_8-sig")
     print(source)
+    return result
